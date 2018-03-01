cdf3a91674853bc5d725a629aa997b78dc1599fc
Aritra Mustafi <aritramustafi56@gmail.com>
2018-03-01 10:57:52 +0530

    second commit

--- MyApplication7/app/src/main/java/com/example/win10/myapplication/MainActivity.java
+++ MyApplication7/app/src/main/java/com/example/win10/myapplication/MainActivity.java
@@ -4,7 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView manutd = (TextView) findViewById(R.id.textView3);
+        if (manutd != null) {
+            manutd.setMovementMethod(LinkMovementMethod.getInstance());
+        }
+        TextView mancity = (TextView) findViewById(R.id.textView2);
+        if (mancity != null) {
+            mancity.setMovementMethod(LinkMovementMethod.getInstance());
+        }
+        TextView chelsea = (TextView) findViewById(R.id.textView4);
+        if (chelsea != null) {
+            chelsea.setMovementMethod(LinkMovementMethod.getInstance());
+        }
+        TextView arsenal = (TextView) findViewById(R.id.textView7);
+        if (arsenal != null) {
+            arsenal.setMovementMethod(LinkMovementMethod.getInstance());
+        }
+        TextView spurs = (TextView) findViewById(R.id.textView5);
+        if (spurs != null) {
+            spurs.setMovementMethod(LinkMovementMethod.getInstance());
+        }
+        TextView liverpool=(TextView)findViewById(R.id.textView6);
+        liverpool.setMovementMethod(LinkMovementMethod.getInstance());
+
     }
     public void manutd(View v)
     {
